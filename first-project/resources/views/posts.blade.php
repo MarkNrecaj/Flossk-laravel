329c9e552de8e26217fc1d0bbc43d9b02f58eedb
Mark <mark.nrecaj0@gmail.com>
2022-12-27 10:25:00 +0100

    show error if inputs are wrong

--- first-project/resources/views/posts.blade.php
+++ first-project/resources/views/posts.blade.php
@@ -9,32 +9,34 @@
 </head>
 
 <body>
-    {{-- @if (Session::has('message'))
-        {{Session::get('message')}}
+    @if (Session::has('message'))
+        {{ Session::get('message') }}
     @endif
 
-    @if ($errors->has('author_id'))
+
+
+    {{-- @if ($errors->has('author_id'))
         {{ $errors->first('author_id') }}
     @endif
 
     @if ($errors->has('author_id'))
-        {{ $errors- >first('author_id') }}
+    {{ $errors- >first('author_id') }}
     @endif
 
     @if ($errors->has('author_id'))
-        {{ $errors->first('author_id') }}
+    {{ $errors->first('author_id') }}
     @endif
 
     @if ($errors->has('author_id'))
-        {{ $errors->first('author_id') }}
+    {{ $errors->first('author_id') }}
     @endif --}}
 
- 
+
 
     {{-- @foreach ($messages->all() as $message)
         {{ $message }}
     @endforeach --}}
-    
+
     <form action="/post" method="post">
         @csrf
         User:<input type="text" name="author_id">
@@ -44,34 +46,49 @@
         Published:<input type="date" name="published">
         <input type="submit">
     </form>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
     <br>
 
-    <table >
+    <table>
         <thead>
-          <th>Author id</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Slug</th>
-          <th>Published</th>
+            <th>Author id</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Slug</th>
+            <th>Published</th>
         </thead>
 
-    @foreach ($posts as $post)
-    <tr>
-        <td>{{$post['author_id']}}</td>
-        <td>{{$post['title']}}</td>
-        <td>{{$post['description']}}</td>
-        <td>{{$post['slug']}}</td>
-        <td>{{$post['published']}}</td>
-      </tr>
-    @endforeach
-        
-      </table>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post['author_id'] }}</td>
+                <td>{{ $post['title'] }}</td>
+                <td>{{ $post['description'] }}</td>
+                <td>{{ $post['slug'] }}</td>
+                <td>{{ $post['published'] }}</td>
+            </tr>
+        @endforeach
+
+    </table>
 
 </body>
 
 </html>
 
-<style>table, th, td {
-    border: 1px solid black;
-    padding: 5px;
-  }</style>
+<style>
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        padding: 5px;
+    }
+</style>
